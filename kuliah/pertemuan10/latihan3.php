9c5c2f0328f5a43282e3b2bcd0e4947a9ec8d370
sulthanjihad <sulthanjihad444@gmail.com>
2020-04-20 23:52:51 +0700

    tambahan

--- kuliah/pertemuan10/latihan3.php
+++ kuliah/pertemuan10/latihan3.php
@@ -17,6 +17,11 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 <body>
 
   <h3>Daftar Mahasiswa</h3>
+
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
+  <br><br>
+
+
   <table border="1" callpadding="10" cellspacing="0">
     <tr>
       <th>#</th>
