c992a5176cf04dabb64cb4a9141533e930e1ca24
sulthanjihad <sulthanjihad444@gmail.com>
2020-05-20 19:50:54 +0700

    sql

--- kuliah/pw_193040048.sql
+++ kuliah/pw_193040048.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Apr 2020 pada 16.05
+-- Waktu pembuatan: 20 Bulan Mei 2020 pada 14.33
 -- Versi server: 10.4.8-MariaDB
 -- Versi PHP: 7.3.11
 
@@ -42,16 +42,38 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`, `gambar`) VALUES
-(1, 'sulthan', '193040048', 'sulthan@gmail.com', 'teknik informatika', 'sulthan.jpg'),
-(2, 'jihad', '193040049', 'jihad@gmail.com', 'teknik informatika', 'jihad.jpg'),
-(3, 'rayhan', '193040050', 'rayhan@gmail.com', 'teknik industri', 'rayhan.jpg'),
-(4, 'abiyyu', '193040051', 'abiyyu@gmail.com', 'teknik mesin', 'abiyyu.jpg'),
-(5, 'aldi', '193040052', 'aldi@gmail.com', 'teknik mesin', 'aldi.jpg'),
-(6, 'ageung', '193040053', 'ageung@gmail.com', 'teknik informatika', 'ageung.jpg'),
-(7, 'dava', '193040054', 'dava@gmail.com', 'teknik pangan', 'dava.jpg'),
-(8, 'putra', '193040055', 'putra@gmail.com', 'teknik lingkungan', 'putra.jpg'),
-(9, 'rais', '193040056', 'rais@gmail.com', 'teknik informatika', 'rais.jpg'),
-(10, 'rafif', '193040057', 'rafif@gmail.com', 'teknik mesin', 'rafif.jpg');
+(1, 'sulthan', '193040048', 'sulthan@gmail.com', 'teknik informatika', '1.png'),
+(2, 'jihad', '193040049', 'jihad@gmail.com', 'teknik informatika', '1.png'),
+(3, 'rayhan', '193040050', 'rayhan@gmail.com', 'teknik industri', '1.png'),
+(4, 'abiyyu', '193040051', 'abiyyu@gmail.com', 'teknik mesin', '1.png'),
+(5, 'aldi', '193040052', 'aldi@gmail.com', 'teknik mesin', '1.png'),
+(6, 'ageung', '193040053', 'ageung@gmail.com', 'teknik informatika', '1.png'),
+(7, 'dava', '193040054', 'dava@gmail.com', 'teknik pangan', '1.png'),
+(8, 'putra', '193040055', 'putra@gmail.com', 'teknik lingkungan', '3.png'),
+(9, 'rais', '193040056', 'rais@gmail.com', 'teknik informatika', '1.png'),
+(10, 'rafif', '193040057', 'rafif@gmail.com', 'teknik mesin', '1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(3, 'pw', '$2y$10$.hjhM4wQDaHz.Ns8gRbtCezXr8iwQ6ZQAqG8LpZ.MTyvAcViai0Ui'),
+(4, 'admin', '$2y$10$kxX.NzfH.3OJ5PLniU/ko.iABY6.8q2f/MTlVnfr2r07Budiv09uq'),
+(5, 'jihad123', '$2y$10$Ta3rQtGQ/oxxo8shgxWMhOtcTqN7vmSxV0ZHkRNHRctCSzaHx.ss6'),
+(7, 'sulthan444', '$2y$10$moL/.cn8Ekr9KElEKKeyh.BazWb2eEIQD05WanT4FZcb7kEF5CLOu');
 
 --
 -- Indexes for dumped tables
@@ -64,6 +86,12 @@ ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -71,7 +99,13 @@ ALTER TABLE `mahasiswa`
 -- AUTO_INCREMENT untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- tubes_193040048.sql
+++ tubes_193040048.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Bulan Mei 2020 pada 14.37
+-- Waktu pembuatan: 10 Apr 2020 pada 16.04
 -- Versi server: 10.4.8-MariaDB
 -- Versi PHP: 7.3.11
 
@@ -44,36 +44,14 @@ CREATE TABLE `buku` (
 INSERT INTO `buku` (`Id`, `Cover`, `NamaBuku`, `Pengarang`, `Penerbit`, `Harga`) VALUES
 (1, '1.jpg', 'more of you', 'Acha sinaga & Andy', 'Elex Media Komputindo', 'Rp72.000'),
 (2, '2.jpg', 'Mariposa', 'Luluk Hf', 'Coconut Books', 'Rp99.000'),
-(3, '3.jpg ', 'Loventure ', 'Indri Octa Safitry ', 'Bhuana Ilmu Populer ', 'Rp59.000 '),
+(3, '3.jpg', 'Loventure', 'Indri Octa Safitry', 'Bhuana Ilmu Populer', 'Rp59.000'),
 (4, '4.jpg', 'Koala and her story', 'Puteri Raka', 'Elex Media Komputindo', 'Rp92.000'),
 (5, '5.jpg', 'Laskar', 'Annisa Fitriani', 'Black swan books', 'Rp97.000'),
 (6, '6.jpg', 'Neraka Dunia', 'Nur St.Iskanda', 'Kepustakaan Populer Gramedia', 'Rp65.000'),
 (7, '7.jpg', 'Selena', 'Tere Liye', 'Gramedia pustaka utama', 'Rp85.000'),
 (8, '8.jpg', 'Bulan dan Bintang', 'Indriya', 'Penerbit Galaxy media', 'Rp95.000'),
 (9, '9.jpg', 'Journal of Terror-titisan-', 'Sweta Kartika', 'M&C!', 'Rp90.000'),
-(10, '10.jpg', 'Pelukis Bisu-The silent Patien', 'Alex Michaelides', 'Gramedia Pustaka Utama', 'Rp85.000'),
-(25, 'selatan.jpg', 'Cerita - Keluh Selatan', 'Sulthan jihad abiyyu', 'on going wattapad', '-');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(64) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `user`
---
-
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin'),
-(2, 'asdasd', '$2y$10$v5zYH1ZsZnTNcsF9zdlSEuTEh192mQB8m5NWx7sEojJijyp5xtY26'),
-(3, 'jihad123', '$2y$10$V4FVqGL4aO58wmxjE3sGxuRmATj3nbEt1VH4JuDuj6AcXeKR5Ta4q');
+(10, '10.jpg', 'Pelukis Bisu-The silent Patien', 'Alex Michaelides', 'Gramedia Pustaka Utama', 'Rp85.000');
 
 --
 -- Indexes for dumped tables
@@ -86,12 +64,6 @@ ALTER TABLE `buku`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indeks untuk tabel `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -99,13 +71,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
-
---
--- AUTO_INCREMENT untuk tabel `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
